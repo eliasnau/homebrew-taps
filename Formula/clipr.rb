5b867acc36cd77dd480dc640dfe4086fde50adb9
Elias <contact@eliasnau.dev>
2025-02-18 21:58:20 +0100

    Update clipr.rb

--- Formula/clipr.rb
+++ Formula/clipr.rb
@@ -1,8 +1,8 @@
 class Clipr < Formula
   desc "A simple and efficient clipboard utility for the command line"
   homepage "https://github.com/eliasnau/clipr"
-  url "https://github.com/eliasnau/clipr/releases/download/v0.1.0/clipr-mac.tar.gz"
-  sha256 "710cd5173e1a9a184a461a2de15ab8b3db5b1c97b6078e935dba1f1b10fdca2b"
+  url "https://github.com/eliasnau/clipr/releases/download/0.1.1/clipr-mac.tar.gz"
+  sha256 "26cc06090d0508af03c9cd082ca3826f19deff1b5c56dff138e4de17c0dfcb53"
   license "MIT"
 
   def install
