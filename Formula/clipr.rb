fa7f1e61bf08ba1262edd144a87903ddd9662957
Elias <contact@eliasnau.dev>
2025-02-18 21:59:51 +0100

    Update clipr.rb

--- Formula/clipr.rb
+++ Formula/clipr.rb
@@ -4,6 +4,7 @@ class Clipr < Formula
   url "https://github.com/eliasnau/clipr/releases/download/0.1.1/clipr-mac.tar.gz"
   sha256 "26cc06090d0508af03c9cd082ca3826f19deff1b5c56dff138e4de17c0dfcb53"
   license "MIT"
+  version "0.1.1"
 
   def install
     bin.install "clipr"
